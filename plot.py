95df49eb29738a32531bfcad27c8772e46f041d6
Rui Zhi Liew <ruizhi@outlook.my>
2023-11-28 01:06:15 +0800

    feat: save plot as image file

--- plot.py
+++ plot.py
@@ -5,15 +5,15 @@ import matplotlib.pyplot as plt
 array_sizes = []
 execution_times = []
 
-for i in range(1000, 1100, 10):
+for i in range(1000000, 1000500, 10):
     time = subprocess.run(['./sequential', str(i)], stdout=subprocess.PIPE).stdout.decode('utf-8').split('\n')[1].split(' ')[2]
 
     array_sizes.append(i)
     execution_times.append(time)
 
+plt.figure(figsize=(15, 20))
 plt.plot(array_sizes, execution_times)
 plt.title('Sequential Sum calculation')
 plt.xlabel('Array size')
 plt.ylabel('Execution time')
-plt.show()
-
+plt.savefig('output/sequential.png')
